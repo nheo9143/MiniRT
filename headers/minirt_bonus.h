84163f374cf17ffe481abbdc291a6120c5ac9a26
Heo Namjun <heonamjun@Heos-MacBook-Air.local>
2023-04-11 16:21:35 +0900

    [Feat] bonus 헤더파일에 size_up 상수 추가

--- headers/minirt_bonus.h
+++ headers/minirt_bonus.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minirt_bonus.h                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: donghyuk <donghyuk@student.42seoul.kr>     +#+  +:+       +#+        */
+/*   By: nheo <nheo@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/21 12:40:54 by nheo              #+#    #+#             */
-/*   Updated: 2022/11/28 16:43:58 by donghyuk         ###   ########.fr       */
+/*   Updated: 2023/04/11 16:01:04 by nheo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@
 
 # define MOVEMENT 0.1
 # define ROTATION 0.1
-
+# define RESIZE 0.1
 typedef enum e_obj
 {
 	SPHERE,
